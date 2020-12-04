ce870da7e2a6ce8df0bd37fbb0588dd9c1fc3f7e
Mariem-design <70693396+Mariem-design@users.noreply.github.com>
2020-12-04 13:35:23 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+ 
+public class Main
+	import java.util.Scanner;
+	
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	String pass;
+	while {
+		System.out.println("Enter password:");
+
+	pass = inputTxt.nextLine();
+	
+	if (pass.equals("marie")) {
+	System.out.println("Hello");
+	}
+	else {
+		System.out.println("password incorrect");
+	}
+	
+	}
+	
+	}
+	
+	// 7 fel som jag inte kan rätta till det ena ligger i scanner.java
+
+
+
+
+	
